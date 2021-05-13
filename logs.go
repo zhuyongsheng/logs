f348dd98e95ae23766e93338be2bc1bde52258a8
zhuyongsheng <yongsheng.zhu@shopee.com>
2021-05-13 12:33:05 +0800

    add log level support

--- logs.go
+++ logs.go
@@ -10,21 +10,107 @@ import (
 @Author : zhuyongsheng
 */
 
-var (
-	Info  *log.Logger // 重要的信息
-	Error *log.Logger // 非常严重的问题
+const (
+	ERROR int = iota
+	WARN
+	INFO
+	DEBUG
 )
 
-func InitLog(fileName string) {
+var (
+	error_ *log.Logger
+	warn   *log.Logger
+	info   *log.Logger
+	debug  *log.Logger
+	Level  int
+)
+
+func InitLogFile(fileName string, level int) {
 	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
-		log.Fatalln("Failed to open error log file:", err)
+		log.Fatalln("Failed to open log file:", err)
 	}
-	Info = log.New(file,
-		"INFO: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-
-	Error = log.New(file,
+	error_ = log.New(file,
 		"ERROR: ",
 		log.Ldate|log.Ltime|log.Lshortfile)
+	warn = log.New(file,
+		"WARN: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+	info = log.New(file,
+		"INFO: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+	debug = log.New(file,
+		"DEBUG: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+	Level = level
+}
+
+func InitLogDirectory(path string, level int) {
+	errorFile, err := os.OpenFile(path+"error_.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("Failed to open error_ log file:", err)
+	}
+	error_ = log.New(errorFile,
+		"",
+		log.Ldate|log.Ltime|log.Lshortfile)
+	warnFile, err := os.OpenFile(path+"warn.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("Failed to open warn log file:", err)
+	}
+	warn = log.New(warnFile,
+		"",
+		log.Ldate|log.Ltime|log.Lshortfile)
+	infoFile, err := os.OpenFile(path+"info.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("Failed to open info log file:", err)
+	}
+	info = log.New(infoFile,
+		"",
+		log.Ldate|log.Ltime|log.Lshortfile)
+	debugFile, err := os.OpenFile(path+"debug.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("Failed to open debug log file:", err)
+	}
+	debug = log.New(debugFile,
+		"",
+		log.Ldate|log.Ltime|log.Lshortfile)
+	Level = level
+}
+
+func Error(format string, v ...interface{}) {
+	if Level >= ERROR {
+		error_.Printf(format, v...)
+	}
+}
+
+func Warn(format string, v ...interface{}) {
+	if Level >= WARN {
+		warn.Printf(format, v...)
+	}
+}
+
+func Info(format string, v ...interface{}) {
+	if Level >= INFO {
+		info.Printf(format, v...)
+	}
+}
+
+func Debug(format string, v ...interface{}) {
+	if Level >= DEBUG {
+		debug.Printf(format, v...)
+	}
+}
+
+// ignore the level set
+func Log(level int, format string, v ...interface{}) {
+	switch level {
+	case ERROR:
+		error_.Printf(format, v...)
+	case WARN:
+		warn.Printf(format, v...)
+	case INFO:
+		info.Printf(format, v...)
+	case DEBUG:
+		debug.Printf(format, v...)
+	}
 }
